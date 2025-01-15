ba5a5395e30c07f794a308f31481d632fa0babbc
edv64 <49319044+EDV64@users.noreply.github.com>
2025-01-15 21:29:07 +0400

    Update mcp3421.cpp

--- mcp3421.cpp
+++ mcp3421.cpp
@@ -1,26 +1,15 @@
-#include "mcp9808.h"
+#include "mcp3421.h"
 #include "esphome/core/log.h"
 
 namespace esphome {
-namespace mcp9808 {
+namespace mcp3421 {
 
-static const uint8_t MCP9808_REG_AMBIENT_TEMP = 0x05;
-static const uint8_t MCP9808_REG_MANUF_ID = 0x06;
-static const uint8_t MCP9808_REG_DEVICE_ID = 0x07;
+static const char *const TAG = "mcp3421";
 
-static const uint16_t MCP9808_MANUF_ID = 0x0054;
-static const uint16_t MCP9808_DEV_ID = 0x0400;
-
-static const uint8_t MCP9808_AMBIENT_CLEAR_FLAGS = 0x1F;
-static const uint8_t MCP9808_AMBIENT_CLEAR_SIGN = 0x0F;
-static const uint8_t MCP9808_AMBIENT_TEMP_NEGATIVE = 0x10;
-
-static const char *const TAG = "mcp9808";
-
-void MCP9808Sensor::setup() {
+void MCP3421Sensor::setup() {
   ESP_LOGCONFIG(TAG, "Setting up %s...", this->name_.c_str());
 
-  uint16_t manu = 0;
+/*  uint16_t manu = 0;
   if (!this->read_byte_16(MCP9808_REG_MANUF_ID, &manu) || manu != MCP9808_MANUF_ID) {
     this->mark_failed();
     ESP_LOGE(TAG, "%s manufacuturer id failed, device returned %X", this->name_.c_str(), manu);
@@ -32,16 +21,19 @@ void MCP9808Sensor::setup() {
     ESP_LOGE(TAG, "%s device id failed, device returned %X", this->name_.c_str(), dev_id);
     return;
   }
+*/
 }
-void MCP9808Sensor::dump_config() {
+
+void MCP3421Sensor::dump_config() {
   ESP_LOGCONFIG(TAG, "%s:", this->name_.c_str());
   LOG_I2C_DEVICE(this);
   if (this->is_failed()) {
     ESP_LOGE(TAG, "Communication with %s failed!", this->name_.c_str());
   }
   LOG_UPDATE_INTERVAL(this);
-  LOG_SENSOR("  ", "Temperature", this);
+//  LOG_SENSOR("  ", "Temperature", this);
 }
+
 void MCP9808Sensor::update() {
   uint16_t raw_temp;
   if (!this->read_byte_16(MCP9808_REG_AMBIENT_TEMP, &raw_temp)) {
@@ -75,7 +67,8 @@ void MCP9808Sensor::update() {
   this->publish_state(temp);
   this->status_clear_warning();
 }
-float MCP9808Sensor::get_setup_priority() const { return setup_priority::DATA; }
+
+float MCP3421Sensor::get_setup_priority() const { return setup_priority::DATA; }
 
 }  // namespace mcp9808
 }  // namespace esphome
