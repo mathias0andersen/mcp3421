787d71df35dd3f47a363e220d04ecd8aa4c19fda
Mathias <133119194+mathias0andersen@users.noreply.github.com>
2025-02-23 21:04:21 +0100

    Update mcp3421.h
    There was two class definitions for MCP3421Sensor. one was removed

--- components/mcp3421/mcp3421.h
+++ components/mcp3421/mcp3421.h
@@ -1,11 +1,18 @@
 #pragma once
 
 #include "esphome/core/component.h"
-#include "esphome/components/sensor/sensor.h"
 #include "esphome/components/i2c/i2c.h"
+#include "esphome/components/sensor/sensor.h"
 
 namespace esphome {
 namespace mcp3421 {
+
+// ✅ CONFIG_SCHEMA is required for ESPHome 2025.2.0+
+static const esphome::schema::Schema CONFIG_SCHEMA = esphome::schema::RootSchema()
+    .Add<int>("sample_width", 18)
+    .Add<bool>("continuous", true)
+    .Add<int>("gain", 1)
+    .Add<int>("update_interval", 60);
 
 enum MCP3421Gain {
   MCP3421_GAIN_1 = 0b00,
@@ -35,40 +42,19 @@ static const uint8_t MCP3421_DEFAULT_ADDR = 0x68;
 
 class MCP3421Sensor : public sensor::Sensor, public PollingComponent, public i2c::I2CDevice {
  public:
+  MCP3421Sensor() : PollingComponent(1000) {}  // Read every second
+
   void setup() override;
   void dump_config() override;
   float get_setup_priority() const override;
   void update() override;
+  
   void set_gain(MCP3421Gain gain) { this->gain_ = gain; }
   void set_resolution(MCP3421Resolution resolution) { this->resolution_ = resolution; }
+
  private:
   MCP3421Gain gain_;
   MCP3421Resolution resolution_;
-};
-
-}  // namespace mcp3421
-}  // namespace esphome
-
-#include "esphome/core/component.h"
-#include "esphome/components/i2c/i2c.h"
-#include "esphome/components/sensor/sensor.h"
-
-namespace esphome {
-namespace mcp3421 {
-
-// This is needed to make ESPHome recognize the component
-static const esphome::schema::Schema CONFIG_SCHEMA = esphome::schema::RootSchema()
-    .Add<int>("sample_width", 18)
-    .Add<bool>("continuous", true)
-    .Add<int>("gain", 1)
-    .Add<int>("update_interval", 60);
-
-class MCP3421Sensor : public esphome::PollingComponent, public esphome::sensor::Sensor, public esphome::i2c::I2CDevice {
- public:
-  MCP3421Sensor() : PollingComponent(1000) {}  // Read every second
-
-  void setup() override;
-  void update() override;
 };
 
 }  // namespace mcp3421
